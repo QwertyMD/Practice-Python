1804268ed77dcfcfe97773dae287d3652dda0d1c
QwertyMD <aaryandhakal2020@gmail.com>
2025-03-17 10:01:12 +0545

    add practice questions

--- week03/exercises/exercise05.py
+++ week03/exercises/exercise05.py
@@ -3,12 +3,13 @@ import numpy as np
 
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([[50, 40, 70, 80, 20], [80, 20, 20, 50, 60], [70, 20, 60, 40, 60], [40, 20, 30, 70, 60]])
-
 plt.title('Bike Details')
-plt.xlabel('Days')
-plt.ylabel('Distance Covered')
+plt.figure(figsize=(10, 8))
+
 for i in range(4):
     j = i + 1
+    plt.xlabel('Days')
+    plt.ylabel('Distance Covered')
     plt.subplot(2, 2, j)
     plt.plot(x, y[i])
 plt.show()
